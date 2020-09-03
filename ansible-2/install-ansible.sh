01d4af5681ec2299bb8257daff4f27780ba9e867
Amine Kahlaoui <amine.kahlaoui@iec.ch>
2020-09-03 15:55:03 +0200

    Edit Scenario

--- ansible-2/install-ansible.sh
+++ ansible-2/install-ansible.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+echo 'PLEASE WAIT A FEW MINUTES'
+echo 'WE ARE GOING TO INSTALL THE LATEST ANSIBLE VERSION'
 python3 --version
 pip3 --version
 pip3 install --upgrade pip
 apt-get update
 pip3 install ansible
+ansible --verion
